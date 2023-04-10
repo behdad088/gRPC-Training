1e9822c051f02398ce459b8e4a409a63b6aebd2e
behdad kardgar <behdad@eliq.io>
2023-04-10 11:41:04 +0200

    add client

--- calculate-average/Client/Program.cs
+++ calculate-average/Client/Program.cs
@@ -1,7 +1,26 @@
-﻿namespace Client
+﻿using Average;
+using Grpc.Core;
+
+namespace Client
 {
     internal class Program
     {
-        static void Main(string[] args) { }
+        const string target = "localhost:50055";
+
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel(target, ChannelCredentials.Insecure);
+
+            await channel.ConnectAsync().ContinueWith((t) =>
+            {
+                if (t.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully.");
+                }
+            });
+
+            var client = new AverageService.AverageServiceClient(channel);
+            channel.ShutdownAsync().Wait();
+        }
     }
 }
