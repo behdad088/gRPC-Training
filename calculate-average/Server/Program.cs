ba48ca2b4ce678b919468c48e992ab17f1671af1
behdad kardgar <behdad@eliq.io>
2023-04-10 11:38:56 +0200

    add server

--- calculate-average/Server/Program.cs
+++ calculate-average/Server/Program.cs
@@ -1,7 +1,35 @@
-﻿namespace grpc.server
+﻿using Grpc.Core;
+
+namespace grpc.server
 {
     internal class Program
     {
-        static void Main(string[] args) { }
+        const int port = 50052;
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine($"The server is listening to port: {port}");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Server faild to start");
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownTask.Wait();
+            }
+        }
     }
 }
