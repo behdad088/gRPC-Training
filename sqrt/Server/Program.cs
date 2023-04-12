08ea15432fc7d2f8bd140b893d231792d99d0120
behdad kardgar <behdad@eliq.io>
2023-04-12 08:09:49 +0200

    implement server code

--- sqrt/Server/Program.cs
+++ sqrt/Server/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+using Max;
+
+namespace sqrt.server
+{
+    internal class Program
+    {
+        const int port = 50057;
+
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Services = { SqrtService.BindService(new SqrtServiceImpl()) },
+                    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine($"The server is listening to port: {port}");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Server faild to start");
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownTask.Wait();
+            }
+        }
+    }
+}
