67c3fd3f9003ffb76108d59eae13fbbbcb26d05f
behdad kardgar <behdad@eliq.io>
2023-04-12 16:51:51 +0200

    Implement client code

--- greet_with_deadline/Client/Program.cs
+++ greet_with_deadline/Client/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Greeting;
+using Grpc.Core;
+
+namespace Client
+{
+    internal class Program
+    {
+        const string target = "localhost:50058";
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel(target, ChannelCredentials.Insecure);
+
+            await channel.ConnectAsync().ContinueWith((t) =>
+            {
+                if (t.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully.");
+                }
+            });
+
+            var client = new GreetingService.GreetingServiceClient(channel);
+
+            try
+            {
+                var response = client.GreetWithDeadline(new GreetingRequest { Name = "Behdad" }, deadline: DateTime.UtcNow.AddMilliseconds(100));
+                Console.WriteLine(response.Result);
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine("Error : " + e.Status.Detail);
+            }
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
