8bdf5fd636cff7b78919994f0c4e1f43dff41b72
behdad kardgar <behdad@eliq.io>
2023-04-12 16:39:19 +0200

    implement server and greetingservice in Server project

--- greet_with_deadline/Server/Program.cs
+++ greet_with_deadline/Server/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Greeting;
+using Grpc.Core;
+
+namespace greeting_with_deadline.server
+{
+    internal class Program
+    {
+        const int port = 50057;
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine($"The server is listening to port: {port}");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Server faild to start");
+                throw;
+            }
+            finally
+            {
+                server?.ShutdownTask.Wait();
+            }
+        }
+    }
+}
